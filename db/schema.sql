9c66189878588dd9da695cc755e0ff470bd01532
Stephen Flores <sflores813@gmail.com>
2020-10-19 12:58:55 -0500

    ran the source schema.sql and seeds.sql could not get schema.sql to work.

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,16 @@
---Create the `burgers_db`.
-  -- * Switch to or use the `burgers_db`.
-  -- * Create a `burgers` table with these fields:
-  --   * **id**: an auto incrementing int that serves as the primary key.
-  --   * **burger_name**: a string.
-  --   * **devoured**: a boolean.
-
--- drops data base if already exists
 DROP DATABASE IF EXISTS burgers_db;
--- creats database
-CREATE DATABASE burgers_db;
--- uses database
-USE burgers_db;
 
--- creates table
+CREATE DATABASE burgers_db;
+
+USE burgers_db
+
 
         CREATE TABLE burgers
 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR (100),
     devoured BOOL NOT NULL,
     
 );
+
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,4 @@
---Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
---  Now you're going to run these SQL files.
 
-  -- * Make sure you're in the `db` folder of your app.
-
-  -- * Start MySQL command line tool and login: `mysql -u root -p`.
-
-  -- * With the `mysql>` command line tool running, enter the command `source schema.sql`. This will run your schema file and all of the queries in it -- in other words, you'll be creating your database.
-
-  -- * Now insert the entries you defined in `seeds.sql` by running the file: `source seeds.sql`.
-
-  -- * Close out of the MySQL command line tool: `exit`.
 
   INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', FALSE); 
   INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Chesse Burger', FALSE);
