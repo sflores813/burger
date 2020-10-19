ae63c95209eeed838e3aadf5bf24b0035da080a2
Stephen Flores <sflores813@gmail.com>
2020-10-19 12:02:43 -0500

    created burger db and table

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,20 @@
   --   * **id**: an auto incrementing int that serves as the primary key.
   --   * **burger_name**: a string.
   --   * **devoured**: a boolean.
+
+-- drops data base if already exists
+DROP DATABASE IF EXISTS burgers_db;
+-- creats database
+CREATE DATABASE burgers_db;
+-- uses database
+USE burgers_db;
+
+-- creates table
+
+        CREATE TABLE burgers
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR (30),
+    devoured VARCHAR (30),
+    
+);
