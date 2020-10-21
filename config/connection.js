a74e24f6e9714bea240ef2c39f224856118a3767
Stephen Flores <sflores813@gmail.com>
2020-10-21 15:14:37 -0500

    added heroku connection

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,18 @@
     password: "",
     database: "burgers_db"
   });
+//heroku conntection
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+ } else {
+      connection = mysql.createConnection({
+         host: "localhost",
+         port: 3306,
+         user: "root",
+         password: " ",
+         database: "burgers_db"
+     });
+ }
   
   // Make connection.
   connection.connect(function(err) {
